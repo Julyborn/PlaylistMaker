ba7650d81e5f4afee44eb4c7a1a92dd6830015b8
peter <peter.hero2000@yandex.ru>
2024-02-29 21:43:26 +0300

    .

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,54 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+    private lateinit var backButton: ImageView
+    private lateinit var clearButton: ImageView
+
+    private val SEARCH_QUERY_STATE_KEY = "search_query"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.construction)
+        setContentView(R.layout.activity_search)
+
+        backButton = findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        clearButton = findViewById(R.id.clearButton)
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
+            val hideKeyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideKeyboard.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            clearButton.visibility = View.GONE
+        }
+
+
+
+        searchEditText = findViewById(R.id.search_edit_text)
+        searchEditText.setText(savedInstanceState?.getString(SEARCH_QUERY_STATE_KEY))
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                savedInstanceState?.putString(SEARCH_QUERY_STATE_KEY, s.toString())
+            }
+        })
     }
 }
